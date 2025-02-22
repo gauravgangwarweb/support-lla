be09e6ca92b7138b4264a957d0d31fb55e127ae7
gauravgangwarweb <gauravgangwar501@gmail.com>
2025-02-22 15:22:21 +0530

    Add new styles and images for the Abonnement section; refactor main.js for improved readability

--- main.js
+++ main.js
@@ -1,65 +1,64 @@
 let nosOpSlider = tns({
-    container: ".nosop-slider",
-    items: 1,
-    controls: false,
-    loop: false,
-    autoplayButtonOutput: false,
-    autoplayHoverPause: false,
-    autoplayResetOnVisibility: false,
-    center: false,
-    touch: true, 
-    mouseDrag: true, 
-    swipeAngle: 30,
-    nav: false,
-    gutter:0,
-    responsive: {
-        0:{
-          items: 1,
-          center: true
-        },
-        786: {
-          items: 3, 
-          center: true
-        },
-        1024: {
-          items: 4, 
-          center:false
-        },
-   
-      },
-  });
+  container: ".nosop-slider",
+  items: 1,
+  controls: false,
+  loop: false,
+  autoplayButtonOutput: false,
+  autoplayHoverPause: false,
+  autoplayResetOnVisibility: false,
+  center: false,
+  touch: true,
+  mouseDrag: true,
+  swipeAngle: 30,
+  nav: false,
+  gutter: 0,
+  responsive: {
+    0: {
+      items: 1,
+      center: true,
+    },
+    786: {
+      items: 3,
+      center: true,
+    },
+    1024: {
+      items: 4,
+      center: false,
+    },
+  },
+});
 
-  document.addEventListener("DOMContentLoaded", () => {
-    const video = document.querySelector("#video");
-    const playButton = document.querySelector("#playBtn");
+document.addEventListener("DOMContentLoaded", () => {
+  const video = document.querySelector("#video");
+  const playButton = document.querySelector("#playBtn");
 
-    playButton.addEventListener("click", () => {
-      if (video.paused) {
-          video.play();
-          playButton.style.display = "none"; // Hide play button when playing
-      } 
+  playButton.addEventListener("click", () => {
+    if (video.paused) {
+      video.play();
+      playButton.style.display = "none"; // Hide play button when playing
+    }
   });
 
   video.addEventListener("click", () => {
-      if (video.paused) {
-          video.play();
-          playButton.style.display = "none"; // Hide play button when playing
-      } else {
-          video.pause();
-          playButton.style.display = "block"; // Show play button when paused
-      }
+    if (video.paused) {
+      video.play();
+      playButton.style.display = "none"; // Hide play button when playing
+    } else {
+      video.pause();
+      playButton.style.display = "block"; // Show play button when paused
+    }
   });
 });
 
 const text = document.querySelector(".scroll-text");
 
-        // Duplicate text for seamless effect
-        const container = document.querySelector(".scroll-container");
-        container.appendChild(text.cloneNode(true));
+// Duplicate text for seamless effect
+const container = document.querySelector(".scroll-container");
+container.appendChild(text.cloneNode(true));
 
-        gsap.to(".scroll-text", {
-            x: "-100%",
-            duration: 10, 
-            ease: "linear",
-            repeat: -1
-        });
+gsap.to(".scroll-text", {
+  x: "-100%",
+  duration: 10,
+  ease: "linear",
+  repeat: -1,
+});
